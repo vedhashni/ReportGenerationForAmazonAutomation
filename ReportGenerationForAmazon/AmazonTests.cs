50bab92d529ad93ce349ea070f9cef7bc664564e
Vedhashni V <vedhashni.v@tvsnext.io>
2021-09-20 10:40:53 +0530

    [Vedhashni] Updated the file with report generation for amazon

---
+++ ReportGenerationForAmazon/AmazonTests.cs
@@ -0,0 +1,36 @@
+﻿/*summary :Report Generation For Amazon Automation 
+  Author: Vedhashni V
+  Date  : 15-09-21
+*/
+
+using AventStack.ExtentReports;
+using NUnit.Framework;
+
+namespace ReportGenerationForAmazon
+{
+    public class AmazonTests:Base.BaseClass
+    {
+        ExtentReports report = Report.report();
+        ExtentTest test;
+
+        //Used to test the tile after launcing the browser
+        [Test, Order(0)]
+        public void TestMethodTitleAfterLaunching()
+        {
+            WebPageAction.SignInAction.TitleAfterLaunching(driver);
+        }
+
+        //Used to read the data from excel
+        //Used to login into Amazon with credentials given in the excel
+        [Test, Order(1)]
+        public void TestMethodForLoginIntoAmazon()
+        {
+            test = report.CreateTest("AmazonTests");
+            test.Log(Status.Info, "Amazon Automation");
+            WebPageAction.SignInAction.ReadDataFromExcel(driver);
+            WebPageAction.SignInAction.LoginIntoAmazon(driver);
+            test.Log(Status.Pass, "TestCases Passed");
+            report.Flush();
+        }
+    }
+}
---
+++ ReportGenerationForAmazon/Report.cs
@@ -0,0 +1,36 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+
+
+namespace ReportGenerationForAmazon
+{
+    public class Report
+    {
+        public static ExtentHtmlReporter htmlReporter;
+        public static ExtentReports extent;
+        public static ExtentTest test;
+
+        public static ExtentReports report()
+        {
+
+            if (extent == null)
+            {
+
+                string reportPath = @"C:\Users\vedhashni.v\source\repos\ReportGenerationForAmazon\ReportGenerationForAmazon\Report\AmazonReport.html";
+                htmlReporter = new ExtentHtmlReporter(reportPath);
+                extent = new ExtentReports();
+                extent.AttachReporter(htmlReporter);
+                extent.AddSystemInfo("OS", "Windows");
+                extent.AddSystemInfo("UserName", "vedhashni");
+                extent.AddSystemInfo("ProviderName", "vedhashni");
+                extent.AddSystemInfo("Domain", "QA");
+                extent.AddSystemInfo("ProjectName", "Amazon Automation");
+
+                string conifgPath = @"C:\Users\vedhashni.v\source\repos\ReportGenerationForAmazon\ReportGenerationForAmazon\Report\extent-config.xml";
+                htmlReporter.LoadConfig(conifgPath);
+            }
+            return extent;
+        }
+
+    }
+}
